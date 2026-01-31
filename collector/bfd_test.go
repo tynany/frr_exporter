24e656f45d3662de6648f8c16ab3aad116e4dacb
Tynan Young <tynany@gmail.com>
2026-01-31 10:26:37 +0400

    Add interface and vrf labels to BFD  metrics

--- collector/bfd_test.go
+++ collector/bfd_test.go
@@ -12,12 +12,12 @@ import (
 
 var expectedBFDMetrics = map[string]float64{
 	"frr_bfd_peer_count{}": 3,
-	"frr_bfd_peer_uptime{local=10.10.141.81,peer=10.10.141.61}": 847716,
-	"frr_bfd_peer_state{local=10.10.141.81,peer=10.10.141.61}":  1,
-	"frr_bfd_peer_uptime{local=10.10.141.81,peer=10.10.141.62}": 847595,
-	"frr_bfd_peer_state{local=10.10.141.81,peer=10.10.141.62}":  1,
-	"frr_bfd_peer_uptime{local=10.10.141.81,peer=10.10.141.63}": 847888,
-	"frr_bfd_peer_state{local=10.10.141.81,peer=10.10.141.63}":  0,
+	"frr_bfd_peer_uptime{iface=eth0,local=10.10.141.81,peer=10.10.141.61,vrf=default}": 847716,
+	"frr_bfd_peer_state{iface=eth0,local=10.10.141.81,peer=10.10.141.61,vrf=default}":  1,
+	"frr_bfd_peer_uptime{iface=eth1,local=10.10.141.81,peer=10.10.141.62,vrf=blue}":    847595,
+	"frr_bfd_peer_state{iface=eth1,local=10.10.141.81,peer=10.10.141.62,vrf=blue}":     1,
+	"frr_bfd_peer_uptime{iface=,local=10.10.141.81,peer=10.10.141.63,vrf=default}":     847888,
+	"frr_bfd_peer_state{iface=,local=10.10.141.81,peer=10.10.141.63,vrf=default}":      0,
 }
 
 func TestProcessBFDPeers(t *testing.T) {
